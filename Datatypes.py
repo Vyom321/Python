79fd6373890241ba1579bd3583923267ce74c205
Vyom321 <137715062+Vyom321@users.noreply.github.com>
2024-01-16 19:15:16 +0530

    Create Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,27 @@
+r=True
+print(type(r))
+#string data types
+str1 = "pythonProgramming"
+str2 = "Simplilearn"
+print(type(str1))
+print(str1[4])
+print(str2[0:4])
+print(str2[-4])
+print(str2.find("m"))
+#List data types
+split1 = "This ,is, a, Orange"
+l1 = split1.split(" ,")
+print(l1)
+#mapping dicitionary
+dict1 = {1:"Lion",2:"Tiger",3:"dog"}
+print(dict1)
+print(type(dict1))
+print(len(dict1))
+#Set data Type
+set1 = {1,3,4,5,6,7,8,0}
+print(type(set1))
+set2 = {'a','b',3,4}
+print(set1|set2)
+print(set2-set1)
+
+
